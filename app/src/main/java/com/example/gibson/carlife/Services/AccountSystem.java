5949ded8ba1a6560fa3df4bcef142932f1e5c753
Gibson <gibsonkong6871@gmail.com>
2018-04-25 17:01:32 +0800

    upload android project

---
+++ app/src/main/java/com/example/gibson/carlife/Services/AccountSystem.java
@@ -0,0 +1,15 @@
+package com.example.gibson.carlife.Services;
+
+/**
+ * Created by gibson on 2018/4/25.
+ */
+
+public class AccountSystem {
+
+  public static boolean login() {
+//    boolean = RequestManager.requestLogin()
+
+    return true;
+  }
+
+}
