d8eef6b4f941dfa581e492d5220ae3e62b14eab8
what211228 <what211228@gmail.com>
2018-06-18 17:20:15 +0800

    half

---
+++ app/src/main/java/com/example/gibson/carlife/View/FavoriteyActivity.java
@@ -0,0 +1,15 @@
+package com.example.gibson.carlife.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gibson.carlife.R;
+
+public class FavoriteyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritey);
+    }
+}
--- app/src/main/java/com/example/gibson/carlife/View/ProductDetailActivity.java
+++ app/src/main/java/com/example/gibson/carlife/View/ProductDetailActivity.java
@@ -8,9 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.gibson.carlife.Abstract.CustomActivity;
 import com.example.gibson.carlife.Adapters.ImagePagerAdapter;
 import com.example.gibson.carlife.MainActivity;
 import com.example.gibson.carlife.Model.DataManagement;
@@ -21,13 +23,15 @@ import com.example.gibson.carlife.Services.Product.ProductPicturesManagement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductDetailActivity extends AppCompatActivity {
+public class ProductDetailActivity extends CustomActivity {
   private static final String TAG = "ProductDetailActivity";
-  //    private static final int[] images = { R.drawable.h01,
-//            R.drawable.h02, R.drawable.h03 };
   public static ArrayList<Bitmap> images;
   private static ImagePagerAdapter imagePagerAdapter;
   private ViewPager viewPager;
+  public TextView type,quanitlty,title,intro,price;
+  public Button buy,cart;
+  public boolean isFavorite;
+
 
   public static void addImage(Bitmap bitmap) {
     images.add(bitmap);
@@ -38,18 +42,20 @@ public class ProductDetailActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_product_detail);
+    isFavorite=false;
 
     int position = getIntent().getIntExtra("position", -1);
     Product item = DataManagement.getProducts().get(position);
 //        Product item = (Product) getIntent().getSerializableExtra("product");
     viewPager = (ViewPager) findViewById(R.id.productImgs_viewPager);
-    TextView type = findViewById(R.id.typeTV);
-    TextView quanitlty  = findViewById(R.id.quanitltyTV);
-    TextView title = findViewById(R.id.titleTV);
-    TextView intro = findViewById(R.id.introTV);
-    TextView price = findViewById(R.id.priceTV);
-    Button buy = findViewById(R.id.buy);
-    Button cart = findViewById(R.id.cart);
+    final ImageView heart = findViewById(R.id.heartIMG);
+    type = findViewById(R.id.typeTV);
+    quanitlty  = findViewById(R.id.quanitltyTV);
+    title = findViewById(R.id.titleTV);
+    intro = findViewById(R.id.introTV);
+    price = findViewById(R.id.priceTV);
+    buy = findViewById(R.id.buy);
+    cart = findViewById(R.id.cart);
     images = new ArrayList<>();
     if(item.product_type!=null)
       type.setText(item.product_type);
@@ -75,7 +81,20 @@ public class ProductDetailActivity extends AppCompatActivity {
       @Override
       public void onClick(View view) {
         //少了Volley
-        Toast.makeText(getBaseContext(), "已放入購物車", Toast.LENGTH_LONG).show();
+        ProductDetailActivity.longTost( "已放入購物車");
+      }
+    });
+    heart.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        ProductDetailActivity.longTost("Test");
+        if(isFavorite){
+            heart.setImageResource(R.drawable.white_heart_fill);
+        }
+        else {
+            heart.setImageResource(R.drawable.white_heart);
+        }
+
       }
     });
 
