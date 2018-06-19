5ca0edfbc096ae646603d9674de63f8d0ea38cd8
what211228 <what211228@gmail.com>
2018-06-19 18:26:08 +0800

    0.0
    偷改圖的解析度<_>

--- app/src/main/java/com/example/gibson/carlife/Services/RequestManager.java
+++ app/src/main/java/com/example/gibson/carlife/Services/RequestManager.java
@@ -2,7 +2,7 @@ package com.example.gibson.carlife.Services;
 
 public abstract class RequestManager {
   public static String host = "http://18.219.196.79";
-  public static int width = 64;
-  public static int height = 64;
+  public static int width = 0;
+  public static int height = 0;
 
 }
